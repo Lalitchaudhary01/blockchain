1d5d88d97481dd1861e6fc9db0cb2fa51f7e59cb
Lalitchdhary01 <Lalitkumar46789@gmail.com>
2025-02-14 21:30:41 +0530

    add wallet

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,4 +5,15 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  define: {
+    global: {},
+  },
+  optimizeDeps: {
+    include: ["buffer"],
+  },
+  resolve: {
+    alias: {
+      buffer: "buffer/",
+    },
+  },
 });
