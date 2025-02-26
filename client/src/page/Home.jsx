c90ed1a81fe11603711782ad3103c36c7a03a4c5
Lalitchdhary01 <Lalitkumar46789@gmail.com>
2025-02-26 10:10:49 +0530

    manage routes

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Sidebar from "./Sidebar";
 import Wallet from "./Wallet";
 import InvestmentCalculator from "./InvestmentCalculator";
 import Dashboard from "./Dashboard";
@@ -7,7 +6,6 @@ import Dashboard from "./Dashboard";
 const Home = () => {
   return (
     <div className="flex bg-[#E5E7EB] min-h-screen">
-      <Sidebar />
       <main className="flex-1 lg:ml-64 h-screen overflow-y-auto">
         <Dashboard />
         <InvestmentCalculator />
